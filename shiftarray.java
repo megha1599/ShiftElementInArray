8ec2b759b7c5a73b42bceec207ea1bcf9cfeef95
megha1599 <71782829+megha1599@users.noreply.github.com>
2021-06-22 12:55:57 +0530

    Add files via upload

---
+++ shiftarray.java
@@ -0,0 +1,32 @@
+class RotateRight {    
+ public static void main(String[] args) {    
+            
+        int [] arr = new int [] {1, 2, 3, 4, 5};     
+        int n = 1;    
+        System.out.println("Main array: ");    
+        for (int i = 0; i < arr.length; i++) {     
+            System.out.print(arr[i] + " ");     
+        }
+        for(int i = 0; i < n; i++)
+        {    
+            int j, last;    
+            
+            last = arr[arr.length-1];    
+            
+            for(j = arr.length-3; j > 0; j--)
+            {    
+                    
+                arr[j] = arr[j-1];    
+            }    
+                
+            arr[0] = last;    
+        }    
+        
+        System.out.println();    
+        System.out.println("\nArray after shift : ");    
+        for(int i = 0; i< arr.length; i++){    
+            System.out.print(arr[i] + " ");    
+        }    
+    }    
+}    
+
